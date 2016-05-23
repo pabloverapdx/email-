99d023fae4f7b227ba7847d98b8ef80fac25fde1
Niem Nguyen and Pablo Vera <pablo@>
2016-05-23 10:57:09 -0700

    initial commit, create gulp, create browserify

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
+gulp.task('jsBrowserify', function() {
+  return browserify({ entries: ['./js/browser.js'] })
+  .bundle()
+  .pipe(source('app.js'))
+  .pipe(gulp.dest('./build/js'));
+});
---
+++ js/message.js
@@ -0,0 +1,9 @@
+exports.Message = function(to, from, messageText){
+  this.to = to;
+  this.from = from;
+  this.messageText = messageText;
+}
+
+exports.Message.prototype.read = function() {
+  return "Dear " + this.to + ", " + this.messageText + " Yours truly, " + this.from;
+}
